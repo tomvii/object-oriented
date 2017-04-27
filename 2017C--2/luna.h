7eacc79c50ecbb0efdd72164474a1fc7f95e3c6b
tomvii <509852661@qq.com>
2017-04-27 16:38:47 +0800

    Create luna.h

---
+++ 2017C--2/luna.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class circle
+{
+public:
+	circle();//Declaration for constructor
+	~circle();////Declaration for destructor
+	double circle_area(int x); //Declaration
+private:
+	int r;
+	double S;
+};
