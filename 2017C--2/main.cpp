26e38c7f0ae5a8ffcc227ec9a3bb550f15e4e9f8
tomvii <509852661@qq.com>
2017-04-27 16:39:12 +0800

    Create main.cpp

---
+++ 2017C--2/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"luna.h"
+using namespace std;
+int main()
+{
+	circle a;
+	int r;
+	double S;
+	cin >> r;
+	S = a.circle_area(r);
+	cout << S << endl;
+	system("pause");
+	return 0;
+}
