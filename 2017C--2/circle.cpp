b8c5ffab94b229db10efba673954f8e6a358828e
tomvii <509852661@qq.com>
2017-04-27 16:39:54 +0800

    Create circle.cpp

---
+++ 2017C--2/circle.cpp
@@ -0,0 +1,19 @@
+#include"luna.h"
+#include<iostream>
+using namespace std;
+double circle::circle_area(int x)
+{
+	double y;
+	y = 3.14*x*x;  //To get the area of circle
+	return y;
+
+}
+circle::circle()
+{
+	r = 0;     //definition
+	S = 0;
+}
+circle::~circle()
+
+
+}
