879c52c1dc31c644c4b14006045b97af42e75c4f
tomvii <509852661@qq.com>
2017-05-07 21:19:41 +0800

    1
    1

---
+++ 2017C--3/calcu.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+void generateExpression(int x);
+void random(int *a, int*b, int*c, int *d);
+void boundary();
+void print();
+void writefile(char str[20]);
+int fileread();
+int test(int total);
+extern int u , z ;
+extern char str1[20];
